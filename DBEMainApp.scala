72d3491ed1ffb9052118ecf9ec3fd491d6551a80
NeruronSpark01 <neuronpark@gmail.com>
2017-07-01 16:38:33 +0530

    Update DBEMainApp.scala

--- DBEMainApp.scala
+++ DBEMainApp.scala
@@ -1,8 +1,12 @@
 package 
 object DBEMainApp {
  def main(args: Array[String]){
+    val configPath:String = args[2];
+    DBEConfigManager.init(configPath) 
     val DBconnObject = new DBConnectionClass
-    DBconnObject.readDataBase
+    val user:String=DBEConfigManager.getValue("CONFIG_DB_INFO").get("uid")
+    val pass:String=DBEConfigManager.getValue("CONFIG_DB_INFO").get("pass")
+    DBconnObject.readDataBase(user=user, pass=pass)
     println("Check DB Is connected or not")
 }
 }
