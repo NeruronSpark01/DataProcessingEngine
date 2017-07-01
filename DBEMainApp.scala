91a59f2e3aed18f1133a85ab26ae7553aa51653a
NeruronSpark01 <neuronpark@gmail.com>
2017-07-01 16:59:32 +0530

    Update DBEMainApp.scala

--- DBEMainApp.scala
+++ DBEMainApp.scala
@@ -3,10 +3,24 @@ object DBEMainApp {
  def main(args: Array[String]){
     val configPath:String = args[2];
     DBEConfigManager.init(configPath) 
-    val DBconnObject = new DBConnectionClass
-    val user:String=DBEConfigManager.getValue("CONFIG_DB_INFO").get("uid")
-    val pass:String=DBEConfigManager.getValue("CONFIG_DB_INFO").get("pass")
-    DBconnObject.readDataBase(user=user, pass=pass)
+    val DBconnObject = new DBConnectionClass;
+    val user:String=DBEConfigManager.getValue("CONFIG_DB_INFO").get("uid");
+    val pass:String=DBEConfigManager.getValue("CONFIG_DB_INFO").get("pass");
+    val url:String= DBEConfigManager.getValue("CONFIG_DB_INFO").get("url");
+    DBconnObject.Init(puser=user, ppass=pass, purl=url);
+  
+    ProcessData(DBconnObject)
+    
+  
     println("Check DB Is connected or not")
 }
+
+def ProcessData():Bool = {
+  // Create ORM for table DPE_ProcessData 
+    // in that table  DATA_PATH VarChsar(500), // "/mywork/data/" Pattern VarChsar(50),
+    
+  // For each 
+  
+    //create spark context here 
+
 }
